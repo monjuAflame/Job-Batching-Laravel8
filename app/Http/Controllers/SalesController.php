f026696d6802b15bd6fb92f395a605f2578d39fb
monjuaflame <monjuaflame@gmail.com>
2021-01-31 23:50:14 +0600

    upload data without file upload in dir path

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -15,42 +15,21 @@ class SalesController extends Controller
     {
     	if (request()->has('mycsv')) {
 
-			// $data = array_map('str_getcsv',file(request()->mycsv));
 			$data = file(request()->mycsv);
-			// dd($data);
-			// $header = $data[0];
-			// // dd($header);
-			// unset($data[0]);
 
 			//chunk 
 			$chunks = array_chunk($data, 1000);
-			// dd($chunk[0]);
-			// convert 1000 record a new csv
-            $path = resource_path('temp');
+
+            $header = [];
 			foreach ($chunks as $key => $chunk) {
-				$name = "/tmp{$key}.csv";
-				// return $path.$name;
-				file_put_contents($path . $name, $chunk);
-			}
-
-        $files = glob("$path/*.csv");
-        // return $files;
-        // dd($files[0]);
-        $header = [];
-        foreach ($files as $key => $file) 
-        {
-            $data = array_map('str_getcsv', file($file));
-            if ($key == 0) {
-                // dd($data);
-                $header = $data[0];
-                // dd($header);
-                unset($data[0]);
+                $data = array_map('str_getcsv', $chunk);
+                if ($key == 0) {
+                    $header = $data[0];
+                    unset($data[0]);
+                }
+                
+                SaleCsvProccess::dispatch($data, $header);
             }
-
-            SaleCsvProccess::dispatch($data, $header);
-            unlink($file);
-
-        }
         return "stored";
 		}
 		return 'couldnot upload';
